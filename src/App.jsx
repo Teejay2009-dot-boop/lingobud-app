24a29789161ef7bb7c50e24cd00a8fa8750c4e58
Faith Abejide <webfortindustries.dev@gmail.com>
2026-01-05 04:20:01 +0100

    Fixed letter casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/NavBar";
+import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Features from "./Components/Features";
 import HowItWorks from "./Components/HowItWorks";
