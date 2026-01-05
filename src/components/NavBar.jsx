c12f4dfafd69a303d39a461d2dc9c0177db2daf1
Teejay2009-dot-boop <abejidefaithtijesunimi@gmail.com>
2026-01-05 05:04:10 +0100

    Rename Navbar component to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default function Navbar() {
+export default function NavBar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const scrollToSection = (sectionId) => {
